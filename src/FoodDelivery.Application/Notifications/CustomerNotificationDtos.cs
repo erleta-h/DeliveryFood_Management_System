5ab22fd1b5710a4ded01525132373486aacf0ecc
RonaHalili <rh70625@ubt-uni.net>
2026-05-07 04:07:19 +0200

    Shtimi i njoftimeve per klientin
    U kriju struktura per njoftimet e klientit dhe push notifications , gjithashtu u pergatit sistemi per njoftime dhe regjistrim të browser-it qe me mund me marrë push messages me vone.

---
+++ src/FoodDelivery.Application/Notifications/CustomerNotificationDtos.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodDelivery.Application.Notifications;
+
+public record CustomerNotificationRowDto(
+    long Id,
+    string Title,
+    string Message,
+    string Type,
+    DateTime CreatedAtUtc,
+    bool IsRead);
