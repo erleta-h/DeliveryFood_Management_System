a836f864f901fdfb7951d7ff8f0b4cb1e7539124
RonaHalili <rh70625@ubt-uni.net>
2026-05-07 03:32:53 +0200

    Implementimi i WebPushSubscription
    U shtua regjistrimi i browser-it per push notifications dhe dergimi i subscription në backend

---
+++ src/FoodDelivery.Application/Realtime/IWebPushSubscriptionService.cs
@@ -0,0 +1,7 @@
+﻿namespace FoodDelivery.Application.Realtime;
+
+public interface IWebPushSubscriptionService
+{
+
+    Task<string?> RegisterAsync(long userId, WebPushSubscribeRequest request, CancellationToken cancellationToken = default);
+}
