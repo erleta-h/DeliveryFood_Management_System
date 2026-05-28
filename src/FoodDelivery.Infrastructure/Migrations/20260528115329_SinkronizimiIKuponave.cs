be78b678ddee649a5ee5a20b4ef2064b7d645dc8
EdisonGegaj <edisongegaj10@gmail.com>
2026-05-28 13:55:32 +0200

    Sinkronizimi i migrimeve lokale

---
+++ src/FoodDelivery.Infrastructure/Migrations/20260528115329_SinkronizimiIKuponave.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodDelivery.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SinkronizimiIKuponave : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Metoda lihet bosh sepse tabelat ekzistojnë në databazën lokale
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Metoda lihet bosh
+        }
+    }
+}
